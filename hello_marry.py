def05e5fb1e60357ec092f7f393295bcd799ac8e
John Mark Lumapac <johnmark.l.lumapac@gmail.com>
2022-05-26 08:44:27 +0800

    adding hello marry

---
+++ hello_marry.py
@@ -0,0 +1,9 @@
+name = input('Enter name')
+password = input('Enter password')
+
+if name == 'marry':
+    print('Hello Marry')
+if password == 'sordfish':
+    print('Access Granted')
+else:
+    print('Wrong Password')
